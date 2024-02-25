f327e1ac30833238b031432ba75c7bb1be8c0e62
Lori Hutchek <Lori.Hutchek@akqa.com>
2024-02-25 18:14:32 +0000

    bst

---
+++ src/binary-search-tree.ts
@@ -0,0 +1,10 @@
+// Practice using binary search trees
+// Below creates a class to manage the creating and editing of BSTs
+class BinaryNode {
+    public left: BinaryNode null;
+    public right = null;
+}
+class BinarySearchTree {
+
+    
+}
